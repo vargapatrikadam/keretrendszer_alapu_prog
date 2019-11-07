8ba0e65e3d5c8b9d650626e98aa0be806c23d7a2
vargapatrikadam <patrikadamvarga@gmail.com>
2019-11-07 10:46:02 +0100

    list all szereplo & add szereplo controller methods

--- controller/src/main/java/filmespelda/controller/SzereploController.java
+++ controller/src/main/java/filmespelda/controller/SzereploController.java
@@ -1,11 +1,16 @@
 package filmespelda.controller;
 
+import filmespelda.model.Szereplo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import service.SzereploService;
+
+import java.util.Collection;
 
 @Controller
 public class SzereploController {
@@ -21,4 +26,16 @@ public class SzereploController {
         return String.valueOf(service.listAllSzereplo().size());
     }
 
+    @RequestMapping(value = "/szereplok", method = RequestMethod.GET)
+    @ResponseBody
+    public Collection<Szereplo> showAllSzereplo(){
+        return service.listAllSzereplo();
+    }
+
+    @RequestMapping(value = "/addSzereplo", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String addSzereplo(@RequestBody Szereplo szereplo){
+        System.out.println(szereplo);
+        return "";
+    }
 }
